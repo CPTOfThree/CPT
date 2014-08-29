9a5f392545533d64265ebeb85afb4a0acabaef5a
CPTOfThree <13600163876@163.com>
2014-08-29 20:50:59 +0800

    准备调整代码结构

--- CPT.Core/CPTPropertyManager.h
+++ CPT.Core/CPTPropertyManager.h
@@ -25,13 +25,13 @@ public:
 	/*
 		注册指定的属性名称和描述，并返回注册的属性对象
 	*/
-	const PCPTPropertyInfo RegisterProperty(std::string propertyName, std::string description);
+	CPT_RESULT RegisterProperty(CPTSTRING propertyName, CPTSTRING description, PCPTPropertyInfo* pCPTPropertyInfo);
 
 	/*
 		添加一个属性变化监视器，指定一个监控回调函数，监控的对象类型,返回一个代表该监控实体的
 		句柄标志callbackHandle
 	*/
-	bool AddPropertyChangedObserver(PCPTPropertyCallback pPropertyCallback, UnitType unitType, CPTUINT* callbackHandle);
+	CPT_RESULT AddPropertyChangedObserver(PCPTPropertyCallback pPropertyCallback, UnitType unitType, CPTUINT* callbackHandle);
 
 	/*
 		通知一个对象书属性发生变化，该接口仅由AutoPropertyUnit对象或者其子类调用
@@ -41,9 +41,13 @@ public:
 	/*
 		注销一个确定的监控器
 	*/
-	bool RemovePropertyChangedObserver(UnitType unitType, CPTUINT callbackHandle);
+	CPT_RESULT RemovePropertyChangedObserver(UnitType unitType, CPTUINT callbackHandle);
+
 
 private:
+
+	_CPT_INITIALIZE_FLAG_FIELD;
+
 	CPTPropertyManager(void);
 	~CPTPropertyManager(void);
 
@@ -51,6 +55,9 @@ private:
 	static CPTPropertyManager* _instance;
 
 	PCPTPropertyInfo _properties;
+	CPTUINT          _registeredCount;
+
+	CPT_RESULT DoRegisterProperty(CPTSTRING propertyName, CPTSTRING description, PCPTPropertyInfo* pPCPTPropertyInfo);
 };
 
 #endif
